a64668a688e67b332d2639d4c190c13114906574
fiyinfholuwa <fiyinfholuwa@gmail.com>
2025-09-24 10:44:28 +0100

    fixies

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PackageController;
 
 Route::post('/webhook/dojah', [PackageController::class, 'handleWebhook']);
+Route::get('/ping', function () {
+    return response()->json([
+        'message' => 'API is working 🚀',
+    ]);
+});
