bdfd32fd39505878fd74ea33cc6aaf84a9c1fbaa
fiyinfholuwa <fiyinfholuwa@gmail.com>
2025-10-04 17:10:03 +0100

    fixies

--- resources/views/auth_new/register.blade.php
+++ resources/views/auth_new/register.blade.php
@@ -149,7 +149,7 @@
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body" style="max-height: 400px; overflow-y: auto;">
-        {{ $settings->login_terms ?? "Not Set" }}
+{!! $settings->login_terms ?? "Not Set" !!}
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
